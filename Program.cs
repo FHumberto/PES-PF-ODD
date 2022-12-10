0700d9b8abb1ecb4a49d1f03b5502c30b256b10d
Humberto Guedes <franciscohguedes@gmail.com>
2022-12-10 17:02:55 -0300

    refact: checagem de extensão de arquivo.

--- Program.cs
+++ Program.cs
@@ -11,48 +11,65 @@
 
             string[] foldersCategory = { "\\Documentos\\", "\\Imagens\\", "\\Música\\", "\\Vídeos\\" };
 
+            // TIPOS DE ARQUIVO MAIS COMUNS
+            List<string> docExtensions = new()
+            { ".doc", ".docx", ".pdf", ".ppt", ".pptx", ".xls", ".txt" };
+            List<string> imgExtensions = new()
+            { ".bmp", ".gif", ".jpg", ".png", ".psd", ".raw", ".svg", ".tiff", ".webp" };
+            List<string> mscExtensions = new()
+            { ".acc", ".ac3", ".flac", ".m4a", ".mp3", ".ogg", ".wav", ".wma" };
+            List<string> vidExtensions = new()
+            { ".3gp", ".avi", ".flv", ".h264", ".m4v", ".mkv", ".mov", ".mp4", ".mpeg", ".mpg", ".mp4", ".mpeg", ".mpg", ".ogv", ".webm", ".wmv" };
+
             try
             {
-                // LISTA TODOS OS ARQUIVOS DO DIRETÓRIO
-                List<string>? allFiles = new(Directory.GetFiles(sourcePath));
-
-                // GERA UM ARRAY IGUAL AO NÚMERO DE PASTAS
-                IEnumerable<string>[] folderFiles = new IEnumerable<string>[foldersCategory.Length];
-
-                // FILTRA OS ARQUIVOS POR CATEGORIA
-                folderFiles[0] = allFiles.Where(a => a.EndsWith(".txt") || a.EndsWith(".pdf") || a.EndsWith(".docx") || a.EndsWith(".ppt") || a.EndsWith(".xls"));
-                folderFiles[1] = allFiles.Where(a => a.EndsWith(".jpeg") || a.EndsWith(".png") || a.EndsWith(".bmp") || a.EndsWith(".gif") || a.EndsWith(".svg"));
-                folderFiles[2] = allFiles.Where(a => a.EndsWith(".mp3") || a.EndsWith(".wav") || a.EndsWith(".acc") || a.EndsWith(".mqa") || a.EndsWith(".ogg"));
-                folderFiles[3] = allFiles.Where(a => a.EndsWith(".mp4") || a.EndsWith(".avi") || a.EndsWith(".mpeg") || a.EndsWith(".mkv") || a.EndsWith(".webm"));
-
-                // CRIA AS PASTAS
-                for (int i = 0; i < foldersCategory.Length; i++)
+                if (string.IsNullOrEmpty(sourcePath))
                 {
-                    if (!Directory.Exists(sourcePath + foldersCategory[i]))
-                    {
-                        Directory.CreateDirectory(sourcePath + foldersCategory[i]);
-                    }
+                    throw new ArgumentException("não pode ser vazio");
+                }
+                else
+                {
+                    // LISTA TODOS OS ARQUIVOS DO DIRETÓRIO
+                    List<string>? allFiles = new(Directory.GetFiles(sourcePath));
 
-                    // VASCULHA CADA CATEGORIA
-                    foreach (var olderPath in folderFiles[i])
-                    {
-                        var newPath = sourcePath + foldersCategory[i] + Path.GetFileName(olderPath);
+                    // GERA UM ARRAY IGUAL AO NÚMERO DE PASTAS
+                    IEnumerable<string>[] folderFiles = new IEnumerable<string>[foldersCategory.Length];
 
-                        if (!File.Exists(olderPath))
+                    // FILTRA OS ARQUIVOS POR CATEGORIA
+                    folderFiles[0] = allFiles.Where(a => docExtensions.Any(x => a.EndsWith(x)));
+                    folderFiles[1] = allFiles.Where(a => imgExtensions.Any(x => a.EndsWith(x)));
+                    folderFiles[2] = allFiles.Where(a => mscExtensions.Any(x => a.EndsWith(x)));
+                    folderFiles[3] = allFiles.Where(a => vidExtensions.Any(x => a.EndsWith(x)));
+
+                    // CRIA AS PASTAS
+                    for (int i = 0; i < foldersCategory.Length; i++)
+                    {
+                        if (!Directory.Exists(sourcePath + foldersCategory[i]))
                         {
-                            // CRIA UM NOVO CAMINHO
-                            using FileStream fs = File.Create(olderPath);
+                            Directory.CreateDirectory(sourcePath + foldersCategory[i]);
                         }
 
-                        if (File.Exists(newPath))
+                        // VASCULHA CADA CATEGORIA
+                        foreach (var olderPath in folderFiles[i])
                         {
-                            Console.WriteLine($"O arquivo {olderPath} já existe {newPath} [não movido]");
-                        }
-                        else
-                        {
-                            // MOVE OS ARQUIVOS.
-                            File.Move(olderPath, newPath, false);
-                            Console.WriteLine($"{olderPath} [movido para] {newPath}.");
+                            var newPath = sourcePath + foldersCategory[i] + Path.GetFileName(olderPath);
+
+                            if (!File.Exists(olderPath))
+                            {
+                                // CRIA UM NOVO CAMINHO
+                                using FileStream fs = File.Create(olderPath);
+                            }
+
+                            if (File.Exists(newPath))
+                            {
+                                Console.WriteLine($"O arquivo {olderPath} já existe {newPath} [não movido]");
+                            }
+                            else
+                            {
+                                // MOVE OS ARQUIVOS.
+                                File.Move(olderPath, newPath, false);
+                                Console.WriteLine($"{olderPath} [movido para] {newPath}.");
+                            }
                         }
                     }
                 }
@@ -60,12 +77,12 @@
             catch (System.IO.DirectoryNotFoundException)
             {
                 Console.WriteLine();
-                Console.Write($"ERRO: Diretório não encontrado");
+                Console.Write($"[ERRO]: Diretório não encontrado");
             }
             catch (System.ArgumentException)
             {
                 Console.WriteLine();
-                Console.WriteLine($"ERRO: O caminho do arquivo não pode ser vazio.");
+                Console.WriteLine($"[ERRO]: O caminho do arquivo não pode ser vazio.");
             }
         }
     }
